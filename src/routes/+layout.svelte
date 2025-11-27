2645c33a197b120817b721c18df1a9ad95449455
DerekTaylor-Gensler <derek_taylor@gensler.com>
2025-11-27 16:20:35 +0000

    feat: update

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,7 @@
 <script>
   import favicon from "$lib/assets/favicon.svg";
   import Portfolio from "$lib/Portfolio.svelte";
+  export const prerender = true;
 
   let { children } = $props();
 </script>
